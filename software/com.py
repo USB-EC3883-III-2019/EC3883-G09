dc9cc80c663545422898f3ffd265a5268c56b0df
jose0796 <jmoran071996@gmail.com>
2019-10-01 23:22:15 -0400

    Adds new serial communication interface script.

---
+++ software/com.py
@@ -0,0 +1,33 @@
+#!python 3.x
+#################################################
+## Author       : Jose Moran
+## Github       : jose0796
+## email        : jmoran071996@gmail.com
+## file         : com.py 
+## Description  : Library for digital and analog 
+## unframing and processing for MC9S08QE128 uC
+#################################################
+
+def openPort(ports,baudrate=115200):
+
+    pass 
+        
+
+
+
+
+def unframeData(frame,channel=1,digital=False):
+
+    pass
+
+
+def synchronize(dataSerial):
+    """ Synchronize data received from serial port
+    """
+    pass
+
+def receiveData(dataSerial):
+    
+    pass
+
+
