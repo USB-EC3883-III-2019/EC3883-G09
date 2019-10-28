5eb1d0b5538e15a95ad51f20b156723080ddbe5f
jose0796 <jmoran071996@gmail.com>
2019-10-28 11:02:28 -0400

    Modifies serial communication

--- software/com.py
+++ software/com.py
@@ -8,26 +8,63 @@
 ## unframing and processing for MC9S08QE128 uC
 #################################################
 
-def openPort(ports,baudrate=115200):
 
-    pass 
+
+def openPort(port="/dev/ttyUSB0",baudrate=115200):
+
+    import serial 
+
+    p = serial.Serial(port, baudrate)
+    return p 
         
-
-
-
-
-def unframeData(frame,channel=1,digital=False):
-
-    pass
-
 
 def synchronize(dataSerial):
     """ Synchronize data received from serial port
     """
-    pass
+    while 1:
+        f = dataSerial.read()
+        if f[0] & 0x80 == 0:
+            for _ in range(3):
+                dataSerial.read()
+            break
+        
+
+
 
 def receiveData(dataSerial):
+    f = dataSerial.read()
+
+    pos = -1
+    s = -1
+    l = -1
+
+
+    if f[7] & 0x80 == 0: 
+        #synchronized already
+        pos = unframeData(f)
+        f = dataSerial.read()
+        s = f << 2
+        f = dataSerial.read()
+        s = s | ((f & 0x60) >> 5)
+        l = (f & 0x1f) << 7
+        f = dataSerial.read()
+        l = l | (f & 0x7f)
+
+        print(pos,s,l)
+
+    else:
+        synchronize(dataSerial)
     
-    pass
+
+
+    
+
+        
+    
+
+    
+    
+
+    
 
 
