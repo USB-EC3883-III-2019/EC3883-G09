c20a3c37c342331446b0ba487fe12b7da8f49f66
Jose <jmoran071996@gmail.com>
2019-11-10 21:51:01 -0400

    Update interface.py

--- software/interface.py
+++ software/interface.py
@@ -24,106 +24,6 @@ import matplotlib.animation as animation
 from com import *
 
 
-class MplCanvas(FigureCanvas):
-    """ 
-    
-        Matplotlib Canvas object for creating plotting window
-
-    """
-
-    def __init__(self, parent=None, width=1, height=1, dpi=100):
-
-        #Plot config
-        self.fig = plt.figure()  
-        FigureCanvas.__init__(self, self.fig)
-        self.axes = self.fig.add_subplot(111, projection="polar")
-        self.axes.set_thetamin(-30)
-        self.axes.set_thetamax(210)
-        #hide x's and y's labels 
-        self.axes.set_xticklabels(np.linspace(0,360,10))
-        self.axes.set_yticklabels([])
-        
-
-        #Grid configuration
-        self.axes.set_facecolor((1,1,1))
-
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(parent) #really important to do this for the gui to show image
-        self.canvas.move(10,40) 
-        
-        self.fig.set_facecolor("none")
-        self.canvas.setStyleSheet("background-color:transparent;")
-        self.axes.set_ylim([0.0, 80.0])
-        self.axes.set_xlim([math.pi*(-30)/180, math.pi*(210)/180])
-
-        
-        self.file = open("solindar.log", "w")
-        self.file.write("Posi\tSonar\tLidar\tFusion\n" )
-        #---------------------------------------------------
-        # Data and serial conf 
-        # --------------------------------------------------
-        self.fusion = {}
-        self.dataSerial = openPort()
-        
-        self.update_data_f = False
-        self.sonar = {}
-        self.lidar = {}
-        
-        
-    
-    def plot(self, channels):
-        
-        """ Closure function for _plot to be call with parameters
-            
-            :returns:
-                A function _plot which calls update_figure.
-
-        """
-        def _plot():
-            self.update_figure(channels)
-        return _plot
-
-    def update_figure(self, channels):
-        self.axes.clear()
-        
-         #empty array to receive data from DEM0QE
-        data = receiveData(self.dataSerial)
-        self.sonar[data[0]] = data[1]
-        self.lidar[data[0]] = data[2]
-        
-        sigma1 = 0.2619**-2
-        sigma2 = 0.6476**-2
-        sigma3 = 1/(sigma1 + sigma2)
-        fus  = (sigma3)*(sigma1*data[1] + sigma2*data[2])
-        self.fusion[data[0]] = fus
-
-
-        
-        self.file.write( str(round(data[0],2)) + "\t" + str(round(data[1],2)) + "\t" 
-                       + str(round(data[2],2)) + "\t" + str(round(fus,2)) + "\n")
-
-        self.axes.set_thetamin(-30)
-        self.axes.set_thetamax(210)
-        self.axes.set_ylim([0.0, 80.0])
-
-
-        #hide x's and y's labels 
-        self.axes.set_yticklabels([])
-        self.axes.set_xticklabels(np.linspace(0,360,10))
-        #self.axes.grid(b=True,which='major',color='r',linestyle='-')
-        #self.axes.minorticks_on()
-        #self.axes.grid(b=True,which='minor',linestyle='--')
-        self.axes.set_facecolor((1,1,1))
-
-        posData = list(self.sonar)
-        sonarData = list(self.sonar.values())
-        lidarData = list(self.lidar.values())
-        sonarlidarData = list(self.fusion.values())
-        if channels[0]: self.axes.scatter(posData, sonarData)
-        if channels[1]: self.axes.scatter(posData, lidarData, color='r')
-        if channels[2]: self.axes.scatter(posData, sonarlidarData, color='y' )
-        self.canvas.draw() #draw
-
 class textBox(QMainWindow):
 
     def __init__(self,mainWindow):
@@ -181,41 +81,68 @@ class Window(QMainWindow):
         self.menuBar().addSeparator()
         self.menuBar().addMenu(self.help_menu)
 
-        #create canvas object 
-        self.canvas = MplCanvas(self)
-
-        #timer configuration for refreshing graph
-        self.timer = None
-
-        #window config
-        self.title = "SONAR/LIDAR"
+        self.title = "Proyecto 2"
         self.top = 100
         self.left = 100
-        self.width = 800 
-        self.height = 600 
+        self.width = 400
+        self.height = 350
         
 
         self.channels = [False,False,False]
         #push button config 
-        button = QPushButton("Start", self) 
-        button.move(600,200)
-        button.clicked.connect(self.plot)
+        button = QRadioButton("Master",self) 
+        button.move(100,50)
+        button.setChecked(True)
+        #button.clicked.connect(self.plot)
 
-        button = QPushButton("Stop", self) 
-        button.move(600,250)
-        button.clicked.connect(self.stop)
+        button = QRadioButton("Slave", self) 
+        button.move(250,50)
+        #button.clicked.connect(self.stop)
 
-        button = QPushButton("LIDAR", self) 
-        button.move(600,300)
-        button.clicked.connect(self.plot_lidar)
-
-        button = QPushButton("SONAR", self) 
-        button.move(600,350)
-        button.clicked.connect(self.plot_sonar)
         
-        button = QPushButton("SOLINDAR", self) 
-        button.move(600,400)
-        button.clicked.connect(self.plot_solindar)
+
+
+        self.t1Tag = QLabel("Torre 1", self)
+        self.t1Tag.move(70,95)
+
+        torre1 = QComboBox(self)
+        torre1.addItem("z1")
+        torre1.addItem("z2")
+        torre1.addItem("z3")
+        torre1.move(120,100)
+        torre1.resize(40,25)
+
+        self.t2Tag = QLabel("Torre 2", self)
+        self.t2Tag.move(70,145)
+        torre2 = QComboBox(self)
+        torre2.addItem("z1")
+        torre2.addItem("z2")
+        torre2.addItem("z3")
+        torre2.move(120, 150)
+        torre2.resize(40,25)
+
+
+        self.t3Tag = QLabel("Torre 3", self)
+        self.t3Tag.move(70,195)
+        torre3 = QComboBox(self)
+        torre3.addItem("z1")
+        torre3.addItem("z2")
+        torre3.addItem("z3")
+        torre3.move(120, 200)
+        torre3.resize(40,25)
+
+        self.t1Tag = QPushButton("Enviar", self)
+        self.t1Tag.resize(50,25)
+        self.t1Tag.move(65,250)
+        textArea = QPlainTextEdit(self)
+        textArea.resize(40,25)
+        textArea.move(120,250)
+        
+        self.t1Tag = QLabel("Recibido", self)
+        self.t1Tag.move(200,95)
+        textArea = QPlainTextEdit(self)
+        textArea.resize(40,25)
+        textArea.move(260,100)
 
         #push button config 
         
