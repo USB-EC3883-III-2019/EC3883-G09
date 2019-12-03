ef4caf23f2b50ee0bad38c1bd957fff3b2ae7dea
jose0796 <jmoran@ubiipagos.com>
2019-12-03 13:06:28 -0500

    Updating interface for project 2

--- software/interface.py
+++ software/interface.py
@@ -24,105 +24,6 @@ import matplotlib.animation as animation
 from com import *
 
 
-class MplCanvas(FigureCanvas):
-    """ 
-    
-        Matplotlib Canvas object for creating plotting window
-
-    """
-
-    def __init__(self, parent=None, width=1, height=1, dpi=100):
-
-        #Plot config
-        self.fig = plt.figure()  
-        FigureCanvas.__init__(self, self.fig)
-        self.axes = self.fig.add_subplot(111, projection="polar")
-        self.axes.set_thetamin(-30)
-        self.axes.set_thetamax(210)
-        #hide x's and y's labels 
-        self.axes.set_xticklabels(np.linspace(0,360,10))
-        self.axes.set_yticklabels([])
-        
-
-        #Grid configuration
-        self.axes.set_facecolor((1,1,1))
-
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(parent) #really important to do this for the gui to show image
-        self.canvas.move(10,40) 
-        
-        self.fig.set_facecolor("none")
-        self.canvas.setStyleSheet("background-color:transparent;")
-        self.axes.set_ylim([0.0, 80.0])
-        self.axes.set_xlim([math.pi*(-30)/180, math.pi*(210)/180])
-
-        
-        self.file = open("solindar.log", "w")
-        self.file.write("Posi\tSonar\tLidar\tFusion\n" )
-        #---------------------------------------------------
-        # Data and serial conf 
-        # --------------------------------------------------
-        self.fusion = {}
-        self.dataSerial = openPort()
-        
-        self.update_data_f = False
-        self.sonar = {}
-        self.lidar = {}
-        
-        
-    
-    def plot(self, channels):
-        
-        """ Closure function for _plot to be call with parameters
-            
-            :returns:
-                A function _plot which calls update_figure.
-
-        """
-        def _plot():
-            self.update_figure(channels)
-        return _plot
-
-    def update_figure(self, channels):
-        self.axes.clear()
-        
-         #empty array to receive data from DEM0QE
-        data = receiveData(self.dataSerial)
-        self.sonar[data[0]] = data[1]
-        self.lidar[data[0]] = data[2]
-        
-        sigma1 = 0.2619**-2
-        sigma2 = 0.6476**-2
-        sigma3 = 1/(sigma1 + sigma2)
-        fus  = (sigma3)*(sigma1*data[1] + sigma2*data[2])
-        self.fusion[data[0]] = fus
-
-
-        
-        self.file.write( str(round(data[0],2)) + "\t" + str(round(data[1],2)) + "\t" 
-                       + str(round(data[2],2)) + "\t" + str(round(fus,2)) + "\n")
-
-        self.axes.set_thetamin(-30)
-        self.axes.set_thetamax(210)
-        self.axes.set_ylim([0.0, 80.0])
-
-
-        #hide x's and y's labels 
-        self.axes.set_yticklabels([])
-        self.axes.set_xticklabels(np.linspace(0,360,10))
-        #self.axes.grid(b=True,which='major',color='r',linestyle='-')
-        #self.axes.minorticks_on()
-        #self.axes.grid(b=True,which='minor',linestyle='--')
-        self.axes.set_facecolor((1,1,1))
-
-        posData = list(self.sonar)
-        sonarData = list(self.sonar.values())
-        lidarData = list(self.lidar.values())
-        sonarlidarData = list(self.fusion.values())
-        if channels[0]: self.axes.scatter(posData, sonarData)
-        if channels[1]: self.axes.scatter(posData, lidarData, color='r')
-        if channels[2]: self.axes.scatter(posData, sonarlidarData, color='y' )
-        self.canvas.draw() #draw
 
 class textBox(QMainWindow):
 
@@ -181,75 +82,72 @@ class Window(QMainWindow):
         self.menuBar().addSeparator()
         self.menuBar().addMenu(self.help_menu)
 
-        #create canvas object 
-        self.canvas = MplCanvas(self)
-
+        
         #timer configuration for refreshing graph
         self.timer = None
 
         #window config
-        self.title = "SONAR/LIDAR"
+        self.title = "PROYECTO 2"
         self.top = 100
         self.left = 100
-        self.width = 800 
-        self.height = 600 
+        self.width = 400 
+        self.height = 400 
+
+        master = QRadioButton("Master", self)
+        master.move(100,100)
+        master.setChecked(True)
+
+        slave = QRadioButton("Slave", self)
+        slave.move(250,100)
+        slave.setEnabled(True)
+
+
+        label = QLabel("Torre 1", self)
+        label.move(50,150)
+        t1 = QComboBox(self)
+        t1.addItem("z1")
+        t1.addItem("z2")
+        t1.addItem("z3")
+        t1.adjustSize()
+        t1.move(100,150)
+
+
+        label = QLabel("Torre 2", self)
+        label.move(50,200)
+        t2 = QComboBox(self)
+        t2.addItem("z1")
+        t2.addItem("z2")
+        t2.addItem("z3")
+        t2.adjustSize()
+        t2.move(100,200)
+
+        label = QLabel("Torre 1", self)
+        label.move(50,250)
+        t3 = QComboBox(self)
+        t3.addItem("z1")
+        t3.addItem("z2")
+        t3.addItem("z3")
+        t3.adjustSize()
+        t3.move(100,250)
         
+        sendText = QPlainTextEdit(self)
+        sendText.resize(40,30)
+        sendText.move(100,300)
 
-        self.channels = [False,False,False]
-        #push button config 
-        button = QPushButton("Start", self) 
-        button.move(600,200)
-        button.clicked.connect(self.plot)
+        send = QPushButton("Enviar",self)
+        send.move(50,300)
+        send.resize(45,30)
 
-        button = QPushButton("Stop", self) 
-        button.move(600,250)
-        button.clicked.connect(self.stop)
+        label = QLabel("Recibido", self)
+        label.move(200,150)
 
-        button = QPushButton("LIDAR", self) 
-        button.move(600,300)
-        button.clicked.connect(self.plot_lidar)
-
-        button = QPushButton("SONAR", self) 
-        button.move(600,350)
-        button.clicked.connect(self.plot_sonar)
-        
-        button = QPushButton("SOLINDAR", self) 
-        button.move(600,400)
-        button.clicked.connect(self.plot_solindar)
-
-        #push button config 
-        
+        received = QPlainTextEdit(self)
+        received.resize(40,30)
+        received.move(260,150)
 
 
         self.setWindowIcon(QtGui.QIcon("icon.png"))
-
-
-
         self.InitWindow()
-
-    def plot_sonar(self):
-        self.channels[0] = not self.channels[0]
-        def __plot():
-            self.canvas.plot(self.channels)
-
-        return __plot
-
-    def plot_lidar(self):
-        self.channels[1] =  not self.channels[1]
-
-        def __plot():
-            
-            self.canvas.plot(self.channels)
-
-        return __plot
-    
-    def plot_solindar(self):
-        self.channels[2] = not self.channels[2]
-        def __plot():
-            self.canvas.plot(self.channels)
-
-        return __plot
-
 
     def InitWindow(self):
         self.setWindowTitle(self.title) #show windows title
